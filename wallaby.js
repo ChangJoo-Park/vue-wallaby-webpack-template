e7c2f0d1d9d5f2f8970d435b6b05a295fe2eafba
ChangJoo Park <pcjpcj2@gmail.com>
2017-04-26 23:51:15 +0900

    Add Wallby to Vue.js webpack template

---
+++ wallaby.js
@@ -0,0 +1,40 @@
+// const webpack = require('webpack')
+const webpackConfig = require('./build/webpack.test.conf.js')
+const wallabyWebpack = require('wallaby-webpack')
+
+module.exports = function (wallaby) {
+  // webpackConfig.ts.compilerOptions = {
+  //   baseUrl: wallaby.localProjectDir,
+  //   paths: {'*': ['./node_modules/*']}
+  // }
+  const wallabyPostprocessor = wallabyWebpack(webpackConfig)
+
+  return {
+    files: [
+      {pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false},
+      {pattern: 'src/**/*.*', load: false}
+    ],
+
+    env: {
+      type: 'browser'
+    },
+
+    tests: [
+      {pattern: 'test/**/*.spec.js', load: false}
+    ],
+
+    postprocessor: wallabyPostprocessor,
+
+    setup: function () {
+      window.__moduleBundler.loadTests()
+    },
+
+    hints: {
+      ignoreCoverage: /ignore coverage/
+    },
+    compilers: {
+      '**/*.js': wallaby.compilers.babel()
+    },
+    debug: true
+  }
+}
